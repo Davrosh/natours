95d267da2405783605dcb743c120954505fc14ad
andy <andy@andy>
2020-11-25 16:18:07 +0000

    Remove names; Fix topology error

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    //useUnifiedTopology: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful'));
 // .catch((err) => console.log('ERROR'));
